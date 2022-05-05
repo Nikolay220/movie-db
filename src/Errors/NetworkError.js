a5c38cf0659d5a8c3033087136ed2bbb631f3fe4
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-05-06 01:32:46 +0700

    update error message

--- src/Errors/NetworkError.js
+++ src/Errors/NetworkError.js
@@ -1,7 +1,6 @@
 export default class NetworkError extends Error {
   constructor(message) {
     super(message)
-    this.checksRecommendations =
-      'Check network connection. If network connection is right and error is stayed, write message with error screenshot to our-company@gmail.com.'
+    this.checksRecommendations = 'Write message with error screenshot to our-company@gmail.com please'
   }
 }
